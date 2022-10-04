4929804e00a738a0bea157a622e9240982d0a94d
Waseem Ashraf <waseemashraf.99@gmail.com>
2022-10-04 15:58:21 +0530

    Initial commit

---
+++ Monthly_Challenges/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Monthly_Challenges project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Monthly_Challenges.settings")
+
+application = get_asgi_application()
