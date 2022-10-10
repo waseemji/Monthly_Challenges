882aaf7c962fd7425606d9ffc79c6052581c135c
Waseem Ashraf <waseemashraf.99@gmail.com>
2022-10-10 11:24:37 +0530

    Finished html elements

--- challenges/urls.py
+++ challenges/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     # path("january/", views.january),
     # path("february",views.february),
+    path("", views.index),
     path("<int:month>", views.monthly_number_challenge),
-    path('<str:month>/',views.monthly_challenge)
+    path('<str:month>/',views.monthly_challenge, name="month-challenge")
 
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from urllib import response
+from django.shortcuts import redirect, render
 from django.http import HttpResponse,HttpResponseNotAllowed, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 challenge_text = {
@@ -17,16 +19,29 @@ challenge_text = {
     "december" : " 2022 is over"
 }
 
+def index(request):
+    redirect_url = ""
+    months = list(challenge_text.keys())
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        redirect_url += f"<li><a href = '{month_path}'>{capitalized_month}</a></li>"
+    response = f"<ul>{redirect_url}</ul>"
+    return HttpResponse(response)
+
 def monthly_number_challenge(request,month):
     months = list(challenge_text.keys())
-    try:
-        return HttpResponseRedirect("/challenges/" + months[month-1])
-    except:
-        return HttpResponseNotFound("Enter a valid month")
+    redirect_month = months[month-1]
+    if month>12:
+        return HttpResponseNotFound("<h1>Enter a valid month</h1>")
+    redirect_path = reverse("month-challenge", args = [redirect_month]) # /challenges/january
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request,month):
     try:
-        return HttpResponse(challenge_text[month])
+        challenge = challenge_text[month]
+        response_data = f"<h1>{challenge}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("Enter a valid month")
+        return HttpResponseNotFound("<h1>Enter a valid month</h1>")
